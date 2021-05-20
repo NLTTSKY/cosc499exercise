d7a112ece327f629bd66b649ffa6cb10b1688caa
tiantian <t.tian981225@gmail.com>
2021-05-19 19:18:09 -0700

    完成项目

---
+++ src/com/company/HeapSort.java
@@ -0,0 +1,74 @@
+package com.company;
+
+class HeapSort{
+	/**
+	 * @len ��¼ÿ��ѭ�����´����鳤��
+	 * @heap ���ܶ����������
+	 * @temp ���ڽ���Ԫ��λ��ʱ�ĸ���ָ��
+	 */
+	private int len;
+	private int[] heap;
+	private int temp;
+	public void setHeap(int[] heap) {
+		this.heap=heap;
+		this.len=heap.length;
+	}
+	
+	public void print() {
+		for (int i = 0; i < heap.length; i++) {
+			System.out.print(heap[i] + ", ");
+		}
+		System.out.println();
+	}
+	
+	public int[] ascOrder() {//�������������������
+		while(len>=2) {
+			//��һ��
+			for(int i=(len/2-1);i>=0 && i<len;i--) {
+				ascNode(i);
+			}
+			heap[0]=heap[len-1];
+			heap[len-1]=temp;
+			len--;	
+			print();
+		}
+		return heap;
+	}
+	public void ascNode(int n) {//��ҳ�ӽڵ�ıȽϺͽ�������
+		temp=heap[n];
+		if((2*n+1)<len && temp<heap[2*n+1]) {
+			heap[n]=heap[2*n+1];
+			heap[2*n+1]=temp;
+			temp=heap[n];
+		}
+		if((2*n+2)<len && temp<heap[2*n+2]) {
+			heap[n]=heap[2*n+2];
+			heap[2*n+2]=temp;
+			temp=heap[n];
+		}
+	}
+	public int[] desOrder() {//С����������齵������
+		while(len>=2) {
+			for(int i=(len/2-1);i>=0 && i<len;i--) {
+				desNode(i);
+			}
+			heap[0]=heap[len-1];
+			heap[len-1]=temp;
+			len--;	
+		}
+		return heap;
+	}
+	public void desNode(int n) {
+		temp=heap[n];
+		if((2*n+1)<len && temp>heap[2*n+1]) {
+			heap[n]=heap[2*n+1];
+			heap[2*n+1]=temp;
+			temp=heap[n];
+		}
+		if((2*n+2)<len && temp>heap[2*n+2]) {
+			heap[n]=heap[2*n+2];
+			heap[2*n+2]=temp;
+			temp=heap[n];
+		}
+	}
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,43 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("初始化成功");
+
+//		int[] arry = {5,6,2,3,9,7,1};
+        int[] arry = {12,2,16,30,8,28,4,10,20,6,18};
+        HeapSort heap = new HeapSort();
+        heap.setHeap(arry);
+        System.out.println(Arrays.toString(heap.ascOrder()));
+        int[] arry1 = {8,4,6,32,54,2,51};
+        heap.setHeap(arry1);
+        System.out.println(Arrays.toString(heap.desOrder()));
+
+        System.out.println(Math.sqrt(784));
+        System.out.println();
+
+        int N = 5;
+        int V = 36;
+        int [] v = new int [N+1];
+        int [] w = new int [N+1];
+        v[0] = 45;
+        v[1] = 28;
+        v[2] = 91;
+        v[3] = 20;
+        v[4] = 10;
+
+        w[0] = 15;
+        w[1] = 10;
+        w[2] = 30;
+        w[3] = 8;
+        w[4] = 7;
+
+        int val[] = new int[] { 45, 28, 91, 20, 10 };
+        int wt[] = new int[] { 15, 10, 30, 8, 7 };
+        int W = 36;
+        int n = val.length;
+        System.out.println(Knapsack.knapSack(W, wt, val, n));
     }
 }
