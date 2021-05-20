15030324a1b96eb409cea985e0e5e39d985efd0d
tiantian <t.tian981225@gmail.com>
2021-05-19 19:20:55 -0700

    提交到分支

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
+         //提交到分值
 //		int[] arry = {5,6,2,3,9,7,1};
         int[] arry = {12,2,16,30,8,28,4,10,20,6,18};
         HeapSort heap = new HeapSort();
